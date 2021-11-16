2f4ae77f40539444a77b0cf9c072818df20adc20
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2021-11-16 23:37:06 +0200

    initial setup4

---
+++ models/TransmitterReceiverDistance.js
@@ -0,0 +1,9 @@
+function TransmitterReceiverDistance(frequency, RSSI){
+    var CLEAR_DISTRACTION_ENVIRONMENT = 2
+    function getParameterN(){
+        return CLEAR_DISTRACTION_ENVIRONMENT
+    }
+    if (frequency == undefined || RSSI == undefined){
+        return
+    }
+}
