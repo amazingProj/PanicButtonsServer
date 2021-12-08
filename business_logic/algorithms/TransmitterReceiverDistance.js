c8dbe0beb2e2510cb0f68cbec68c34469f796ac8
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2021-12-08 16:15:18 +0200

    starting a new edge

--- business_logic/algorithms/TransmitterReceiverDistance.js
+++ business_logic/algorithms/TransmitterReceiverDistance.js
@@ -1,9 +1,38 @@
+/**
+ * function gets a frequency and rssi and return distance
+ * @param {*} frequency 
+ * @param {*} RSSI 
+ * @returns the distance from the transmitter device(double)
+ */
 function TransmitterReceiverDistance(frequency, RSSI){
-    var CLEAR_DISTRACTION_ENVIRONMENT = 2
+    /**constants */
+    const ONE_METER_RSSI = 27.55
+    const CLEAR_DISTRACTION_ENVIRONMENT_N = 2
+    const LOGARITHM_BASIS = 10
+    const DEFAULT_TO_FIXED_ANSWER = 3
+
     function getParameterN(){
-        return CLEAR_DISTRACTION_ENVIRONMENT
+        return CLEAR_DISTRACTION_ENVIRONMENT_N
     }
+
+    function getLogNumber(number){
+        return Math.log10(number) * LOGARITHM_BASIS
+    }
+
+    function getFractionToFixed(numerator, denominator, toFix = DEFAULT_TO_FIXED_ANSWER){
+        let num = numerator / denominator
+        return Math.toFixed(toFix)
+    }
+
     if (frequency == undefined || RSSI == undefined){
         return
     }
+
+    let exponentDenominator = CLEAR_DISTRACTION_ENVIRONMENT_N * LOGARITHM_BASIS
+    let exponentNumerator = ONE_METER_RSSI - (getLogNumber(frequency) + RSSI)
+    let fraction = getFractionToFixed(exponentNumerator, exponentDenominator)
+
+    return Math.pow(fraction, 10)
 }
+
+module.exports = TransmitterReceiverDistance
---
+++ business_logic/unittests/TransmitterReceiverDistanceTest.js
@@ -0,0 +1,17 @@
+var expect = require("expect")
+
+var TransmitterReceiverDistance = require('../algorithms/TransmiterReciever.js')
+
+var assert = require('assert')
+
+describe('Normal cases', () => {
+    describe('two test cases', () => {
+        it('First Case', () => {
+            let frequency = 2.5
+            let rssi = 40
+
+            let distance = TransmitterReceiverDistance(frequency, rssi)
+            expect(distance).toEquals(5)
+        })
+    })
+})
