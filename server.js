b3dd1189f9007a669433dcef473ee8d2443c0a55
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2021-12-30 10:23:39 +0200

    This is working with seperation the routes in server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express')
 //requires socket.io module
 const socket = require('socket.io')
 
+const socketIoEvents = require('./routes/SocketIoRouteEvents')
+
 const app = express()
 
 /** ***************** environment variables ******************* */
@@ -21,28 +23,4 @@ console.log("Server is running");
 const io = socket(server);
 
 //Socket.io Connection------------------
-io.on("connection", (socket) => {
-    console.log("New socket connection: " + socket.id)
-
-    /********************* events ********************* */
-
-    socket.on("macAddress", (data) => {
-      console.log(data);
-    });
-
-    socket.on("wifiInformation", (data) => {
-        console.log(data);
-      });
-  
-    socket.on("panic-signal", (data) => {
-      console.log("emergency signal received " + data + "\n");
-    });
-  
-    socket.on("esp32wifi-info", (data) => {
-      console.log(data);
-    });
-
-    socket.on("message", (data) => {
-      console.log(data);
-    });
-  });
+io.on("connection", socketIoEvents.events);
