33a372b0aece4fa4a54c7cd1709a268d9854f5bb
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-01-19 09:13:18 +0200

    mqtt cloud service

--- routes/SocketToModel.js
+++ routes/SocketToModel.js
@@ -1,7 +1,6 @@
 const InformationHandler = require('../models/SocketIoInformation/InfomationHandler')
 
 exports.SendData = function (data){
-  console.log(data)
   let handler = InformationHandler.socketIoHandler
   let response = handler(data)
 }
--- server.js
+++ server.js
@@ -24,3 +24,36 @@ const io = socket(server);
 
 //Socket.io Connection------------------
 io.on("connection", socketIoEvents.events);
+
+var mqtt = require('mqtt')
+
+var options = {
+    host: '965b05d8eb154cfd8dd2a50aed2d5655.s1.eu.hivemq.cloud',
+    port: 8883,
+    protocol: 'mqtts',
+    username: 'Esp32',
+    password: 'Esp32Asaf'
+}
+
+//initialize the MQTT client
+var client = mqtt.connect(options);
+
+//setup the callbacks
+client.on('connect', function () {
+    console.log('Connected');
+});
+
+client.on('error', function (error) {
+    console.log(error);
+});
+
+client.on('message', function (topic, message) {
+    //Called each time a message is received
+    console.log('Received message:', topic, message.toString());
+});
+
+// subscribe to topic 'my/test/topic'
+client.subscribe('my/test/topic');
+
+// publish message 'Hello' to topic 'my/test/topic'
+client.publish('my/test/topic', 'Hello');
